b2330c5d93d6e7d868bf89526243dce653394ddf
CaioLira18 <caiozao1212@gmail.com>
2024-02-18 18:05:17 -0300

    Project Girl Group Website

---
+++ js/members.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('background-video');
+    video.play();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('background-video_2');
+    video.play();
+});
